af021d739c21d8d653f74f011db3d964ed00d074
LakoR15 <alexvolkov1994@gmail.com>
2016-01-13 19:35:06 +0300

    1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,6 @@
-import org.springframework.web.client.RestTemplate;
 import slack.Message;
-import slack.SlackUser;
-import slack.SlackUserListParser;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Application {
 
@@ -12,24 +9,44 @@ public class Application {
 
         String user = "maxim_nikitin";
         String token = "xoxb-18373787971-0b7ajI9mNSOnbswAwIK0sdvW";
-        String text = "Hellow World";
-        Message message = new Message();
-        message.sendMessage(user,text,token);
 
+        do {
+            System.out.print("Введите сообщение: ");
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
+            Message message = new Message();
+            message.sendMessage(user, text, token);
+        }while (true);
+
+//        String user = "maxim_nikitin";
+//        String token = "xoxb-18373787971-0b7ajI9mNSOnbswAwIK0sdvW";
+//        String text = "Hellow World";
+//        Message message = new Message();
+//        message.sendMessage(user,text,token);
+//
 //        String uri = "https://slack.com/api/chat.postMessage";
 //        String body = "?token=xoxb-18373787971-0b7ajI9mNSOnbswAwIK0sdvW&channel=@alexander_volkov&text=Русский+текст";
-        String uri = "https://slack.com/api/users.list?token=xoxb-18373787971-0b7ajI9mNSOnbswAwIK0sdvW&pretty=1";
-        String str = "";
-
-        RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, String.class);
-
-        SlackUserListParser slackUserListParser = new SlackUserListParser(result);
-        ArrayList<SlackUser> slackUsers = new ArrayList<SlackUser>();  
-        slackUsers.addAll(slackUserListParser.getListSlackUser());
-
-        for (SlackUser slackUser: slackUsers) {
-            System.out.println(slackUser.getId() + "\n" + slackUser.getTeamId() + "\n" + slackUser.getName() + "\n");
-        }
+//        String uri = "https://slack.com/api/users.list?token=xoxb-18373787971-0b7ajI9mNSOnbswAwIK0sdvW&pretty=1";
+//        String str = "";
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        RestTemplate restTemplate = new RestTemplate();
+//        String result = restTemplate.getForObject(uri, String.class);
+//        try {
+//            List<SlackUser> slackUsers = mapper.readValue(result, new TypeReference<List<SlackUser>>(){});
+//            System.out.println(slackUsers.get(1).toString());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//        SlackUserListParser slackUserListParser = new SlackUserListParser(result);
+//        ArrayList<SlackUser> slackUsers = new ArrayList<SlackUser>();
+//        slackUsers.addAll(slackUserListParser.getListSlackUser());
+//
+//        for (SlackUser slackUser: slackUsers) {
+//            System.out.println(slackUser.getId() + "\n" + slackUser.getTeamId() + "\n" + slackUser.getName() + "\n");
+//        }
     }
 }
--- src/main/java/slack/SlackUser.java
+++ src/main/java/slack/SlackUser.java
@@ -1,15 +1,51 @@
 package slack;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class SlackUser {
 
-    String id;
-    String teamId;
-    String name;
+    @JsonProperty
+    private String id;
+    @JsonProperty(value = "team_id")
+    private String teamId;
+    @JsonProperty
+    private String name;
+    private boolean deleted;
+    private String status;
+    private String color;
+    @JsonProperty
+    private String realName;
+    private String tz;
+    @JsonProperty(value = "tz_label")
+    private String tzLabel;
+    @JsonProperty(value = "tz_offset")
+    private int tzOffset;
+    private String profile;
+    @JsonProperty(value = "is_admin")
+    private boolean isAdmin;
+    @JsonProperty(value = "is_owner")
+    private boolean isOwner;
+    @JsonProperty(value = "is_primary_owner")
+    private boolean isPrimaryOwner;
+    @JsonProperty(value = "is_restricted")
+    private boolean isRestricted;
+    @JsonProperty(value = "is_bot")
+    private boolean isBot;
+
 
     public SlackUser(String id, String teamId, String name) {
         this.id = id;
         this.teamId = teamId;
         this.name = name;
+    }
+
+    @Override
+    public String toString() {
+
+        return getId() + "\n" +
+                getName() + "\n" +
+                getTeamId() + "\n" +
+                getRealName();
     }
 
     public String getId() {
@@ -22,5 +58,57 @@ public class SlackUser {
 
     public String getName() {
         return name;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public String getTz() {
+        return tz;
+    }
+
+    public String getTzLabel() {
+        return tzLabel;
+    }
+
+    public int getTzOffset() {
+        return tzOffset;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public boolean isOwner() {
+        return isOwner;
+    }
+
+    public boolean isPrimaryOwner() {
+        return isPrimaryOwner;
+    }
+
+    public boolean isRestricted() {
+        return isRestricted;
+    }
+
+    public boolean isBot() {
+        return isBot;
     }
 }
